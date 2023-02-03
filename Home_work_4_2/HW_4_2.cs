c899311e8b35cbb0673cc2a15c7daac3fd95312a
Андрей Будяну <andre40000k@gmail.com>
2023-02-03 20:09:37 +0200

    Update

--- Home_work_4_2/HW_4_2.cs
+++ Home_work_4_2/HW_4_2.cs
@@ -4,7 +4,72 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // Task 2
+            // Пользователь вводит с клавиатуры два числа.
+            // Нужно посчитать отдельно сумму четных, нечетных и чисел,
+            // кратных 9 в указанном диапазоне, а также среднеарифметическое каждой группы.
+
+            Console.WriteLine("Пользователь вводит с клавиатуры два числа(начало и конец диапазона)." +
+                "\r\n1. Нужно посчитать отдельно сумму четных;" +
+                "\n2. Нужно посчитать отдельно сумму нечетных;" +
+                "\n3. Нужно посчитать отдельно сумму кратных 9;" +
+                "\n4. Tакже среднеарифметическое каждой группы.");
+
+            const int multiplicityOfTwo = 2, multiplicityOfNine = 9;
+
+            int rengeStart = 0, rengeEnd = 0, reverseOrder, multiplesOfTwo = 0, multiplesNotTwo = 0, multiplesOfNine = 0,
+             numberMultiplesTwo = 0, numberMultiplesNotTwo = 0, numberMultiplesOfNine = 0;
+
+            try
+            {
+                Console.Write("\nEnter start of range: ");
+                rengeStart = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter end of range: ");
+                rengeEnd = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nErrors!\nIncorrd data entry");
+                return;
+            }
+
+            Console.Write("\nT0" +
+                "\t T1" +
+                "\t T2" +
+                "\t T3\n");
+
+            reverseOrder = rengeEnd;
+
+            for (int i = rengeStart; i <= rengeEnd; i++)
+            {
+                Console.WriteLine($"{i} " +
+                    $"\t {(i % multiplicityOfTwo == 0 ? i : "-")}" +
+                    $"\t {(i % multiplicityOfTwo != 0 ? i : "-")}" +
+                    $"\t {(i % multiplicityOfNine == 0 ? i : "-")}");
+
+
+                multiplesOfTwo = i % multiplicityOfTwo == 0 ? (multiplesOfTwo += i) : multiplesOfTwo;
+                numberMultiplesTwo = i % multiplicityOfTwo == 0 ? ++numberMultiplesTwo : numberMultiplesTwo;
+
+                multiplesNotTwo = i % multiplicityOfTwo != 0 ? (multiplesNotTwo += i) : multiplesNotTwo;
+                numberMultiplesNotTwo = i % multiplicityOfTwo != 0 ? ++numberMultiplesNotTwo : numberMultiplesNotTwo;
+
+                multiplesOfNine = i % multiplicityOfNine == 0 ? (multiplesOfNine += i) : multiplesOfNine;
+                numberMultiplesOfNine = i % multiplicityOfNine == 0 ? ++numberMultiplesOfNine : numberMultiplesOfNine;
+            }
+
+            Console.WriteLine('\n' + new string('-',80));
+
+            Console.WriteLine($"\nSum:\t {multiplesOfTwo}" +
+                $"\t {multiplesNotTwo}" +
+                $"\t {multiplesOfNine}");
+
+            Console.WriteLine($"\nT4\nAverage: {(double)multiplesOfTwo / numberMultiplesTwo}" +
+                $"\t {(double)multiplesNotTwo / numberMultiplesNotTwo}" +
+                $"\t {(double)multiplesOfNine / numberMultiplesOfNine}");
         }
     }
 }
