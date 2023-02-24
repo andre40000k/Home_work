5fb81844b1fc2bbf3fe3215456915b49538abf30
Андрей Будяну <andre40000k@gmail.com>
2023-02-24 19:00:39 +0200

    ДЗ 10. Коллекции

---
+++ Home_work_10/HW_10.cs
@@ -0,0 +1,97 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Home_work_10
+{    
+    internal class HW_10
+    {
+        
+        static void Main(string[] args)
+        {
+            // HW_10
+            // Создать программу "список дел". (List)
+            // Реализовать:
+            //   - добавление задачи в список задач
+            //   - изменение задачи(найти по индексу)
+            //   - удаление задачи(найти по индексу)
+            //Задача представляет из себя строчку(string).
+
+            Random rnd = new Random();
+
+            List<string> tasks = new List<string>() { "Task_1", "Task_2", "Task_3" };
+
+            foreach (var item in tasks)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine('\n' + new string('=', 80) + '\n');
+
+            // Add
+
+            for (int i = 4; i <= 6; i++)
+            {
+                tasks.Add($"Task_{i}");
+            }
+
+            foreach (var item in tasks)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine('\n' + new string('=', 80) + '\n');
+
+            // Change            
+
+            int rnd_index = rnd.Next(0, tasks.Count());
+
+            string rnd_task = "Change_" + tasks[rnd_index];
+
+            //V1
+            //tasks.RemoveAt(rnd_index);
+            //tasks.Insert(rnd_index, rnd_task);
+
+
+            //V2
+            tasks[rnd_index] = rnd_task;
+
+            foreach (var item in tasks)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine('\n' + new string('=', 80) + '\n');
+
+            // Remove
+
+            rnd_index = rnd.Next(0, tasks.Count());
+
+            tasks.RemoveAt(rnd_index);
+
+            foreach (var item in tasks)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine('\n' + new string('=', 80) + '\n');
+
+
+            //var names = new List<string>() {
+            //    "Vasya",
+            //    "Petya",
+            //    "Anton",
+            //    "qq",
+            //    "www",
+            //    "e",
+            //    "rrrr"
+            //};
+
+            //names[names.Count() - 1] = "wwwwwww";
+            //Console.WriteLine(names[names.Count() - 1]);
+
+            //foreach (var item in names)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+    }
+}
